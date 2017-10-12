60dd5968731eb22eaca8e4b2b80848d5e1f4fe8c
wufang <naichunyu@aliyun.com>
2017-10-12 20:25:25 +0800

    添加注释：dispatch相当于调用reducer函数

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import {startServer} from './src/server';//引入Socket.io server
  * 整体流程：
  * 一、客户端向服务器发送指定的action
  * 二、服务器将操作权交给Redux Store
- * 三、Redux Store调用reducer并且reducer执行与action相关的逻辑。
+ * 三、Redux Store调用reducer[通过dispatch]并且reducer执行与action相关的逻辑。
  * 四、Redux Store更新状态，基于reducer返回的值
  * 五、Redux Store执行被服务器subscribe订阅的监听函数
  * 六、服务器发出'state'事件
