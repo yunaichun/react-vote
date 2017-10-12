b33c54a39b9446909b14ac84dd86e37df0458849
wufang <naichunyu@aliyun.com>
2017-10-12 17:41:24 +0800

    订阅Redux Store，当状态改变时服务端立即发送Redux Store最新状态给Socket.io客户端。And有客户端连接同样操作。

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import makeStore from './src/store';
+import makeStore from './src/store';//引入Redux Store
+import {startServer} from './src/server';//引入Socket.io server
 /**
  * [store 导出一个store]
  */
 export const store = makeStore();
+startServer(store);
--- src/server.js
+++ src/server.js
@@ -2,7 +2,26 @@
 //它还有一些不支持WebSockets的客户端的后备机制。
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
 	//端口需要匹配从客户端链接的端口
 	const io = new Server().attach(8090);
+
+	/**
+	 * 订阅Redux Store，我们就可以知道什么时候Redux Store的状态改变了
+	 */
+	store.subscribe(
+		/**
+		 * [---状态发生变化时：立即发送状态快照---]
+		 * Socket.io服务端触发state事件，结果是发送Redux Store的JSON序列化状态给所有连接的Socket.io客户端
+		 */
+		()=>io.emit('state',store.getState().toJS())
+	);
+
+	/**
+	 * [---有新的客户端连接时:立即发出当前状态---]
+	 * Socket.io服务端触发state事件，结果是发送Redux Store的JSON序列化状态给所有连接的Socket.io客户端
+	 */
+	io.on('connection', (socket) => {
+	    socket.emit('state', store.getState().toJS());
+	})
 }
