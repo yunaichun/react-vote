62041283c9e277dadd87cb31859f4450ba6776da
wufang <naichunyu@aliyun.com>
2017-10-12 20:21:19 +0800

    服务端监听'action'事件调用dispatch改变Redux Store状态，客户端发起投票事件可以触发服务器的'action'事件，这样就可以实现客户端触发服务端改变Redux Store的状态

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 import makeStore from './src/store';//引入Redux Store
 import {startServer} from './src/server';//引入Socket.io server
+
 /**
- * [store 导出一个store]
+ * 整体流程：
+ * 一、客户端向服务器发送指定的action
+ * 二、服务器将操作权交给Redux Store
+ * 三、Redux Store调用reducer并且reducer执行与action相关的逻辑。
+ * 四、Redux Store更新状态，基于reducer返回的值
+ * 五、Redux Store执行被服务器subscribe订阅的监听函数
+ * 六、服务器发出'state'事件
+ * 七、所有连接的客户端（包括启动原始操作的客户端）接收Redux Store最新的状态
  */
 export const store = makeStore();
 startServer(store);
+
+
+
+/**
+ * 通过dispatch调用reducer
+ */
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')//加载投票条目
+});
+store.dispatch({type: 'NEXT'});//通过发送Next类型的action去触发投票
--- src/server.js
+++ src/server.js
@@ -2,7 +2,18 @@
 //它还有一些不支持WebSockets的客户端的后备机制。
 import Server from 'socket.io';
 
-export default function startServer(store) {
+
+/**
+ * 整体流程：
+ * 一、客户端向服务器发送指定的action
+ * 二、服务器将操作权交给Redux Store
+ * 三、Redux Store调用reducer并且reducer执行与action相关的逻辑。
+ * 四、Redux Store更新状态，基于reducer返回的值
+ * 五、Redux Store执行被服务器subscribe订阅的监听函数
+ * 六、服务器发出'state'事件
+ * 七、所有连接的客户端（包括启动原始操作的客户端）接收Redux Store最新的状态
+ */
+export function startServer(store) {
 	//端口需要匹配从客户端链接的端口
 	const io = new Server().attach(8090);
 
@@ -14,7 +25,7 @@ export default function startServer(store) {
 		 * [---状态发生变化时：立即发送状态快照---]
 		 * Socket.io服务端触发state事件，结果是发送Redux Store的JSON序列化状态给所有连接的Socket.io客户端
 		 */
-		()=>io.emit('state',store.getState().toJS())
+		()=>io.emit('state',store.getState().toJS())//获取Redux Store当前状态
 	);
 
 	/**
@@ -23,5 +34,10 @@ export default function startServer(store) {
 	 */
 	io.on('connection', (socket) => {
 	    socket.emit('state', store.getState().toJS());
+	    /**
+	     * 客户端触发后【客户端发送此"action"】
+	     * 服务器接收到后【监听"action"事件】，然后去改变服务端Redux Store的状态
+	     */
+	    socket.on('action', store.dispatch.bind(store));//dispatch是调用Redux Stored的reducer
 	})
 }
