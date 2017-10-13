a703aed5d6bd0fc4c072307528084ac8542fa310
wufang <naichunyu@aliyun.com>
2017-10-13 12:52:55 +0800

    配置webpack的entry、output、babel-loader、devServer

---
+++ src/index.js
@@ -0,0 +1,5 @@
+var path=require('path');
+console.log(__filename);
+console.log(__dirname);
+console.log(path.join(__dirname, "dist"));
+console.log('I am alive!');
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports={
+	entry:[
+	'./src/index.js'
+	],
+	output:{
+		path:__dirname+'/dist',
+		publicPath:'/',//设置网站跟目录【就不用打包图片等资源了】
+		filename:'bundle.js'
+	},
+	resolve:{
+		extensions: ['.js', '.jsx']
+	},
+	module:{
+		loaders:[{
+			test:/\.jsx?$/,
+			exclude:/node_modules/,
+			loader:'babel-loader'
+		}]
+	},
+	devServer: {
+	    contentBase: './dist'
+	}
+}
