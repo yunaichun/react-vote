5d93af9ce0bb793286f51f9c7e315faf0708445e
wufang <naichunyu@aliyun.com>
2017-10-13 14:34:43 +0800

    添加测试框架：mocha、chai、jsdom

---
+++ test/test_helper.js
@@ -0,0 +1,22 @@
+/**
+ * 接口文档：https://www.npmjs.com/package/jsdom
+ */
+import jsdom from 'jsdom';
+const doc = new jsdom.JSDOM('<!doctype html><html><body></body></html>');
+const win = doc.window;
+
+/**
+ * 将jsdom创建的document和window暴露在全局
+ */
+global.document = doc;
+global.window = win;
+
+
+/**
+ * 将jsdom window对象包含的属性放在nodejs环境的document对象中
+ */
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
