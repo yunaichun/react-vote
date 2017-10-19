a2833402135ba6466c437cfb85456a98c496ebb5
wufang <naichunyu@aliyun.com>
2017-10-19 17:22:39 +0800

    解决许久没解决的React-Router4.2路由配置问题【看官方文档啊啊啊啊啊】

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,17 @@
 /**引入React**/
 import React from 'react';
+/*引入Voting投票组件*/
+import Voting from './Voting'
 /**引入immutable**/
 import {List} from 'immutable';
-
-
 const pair = List.of('Trainspotting', '28 Days Later');
+
 
 export default class App extends React.Component{
 	constructor(props){
 		super(props);
 	}
     render() {
-    	console.log(this);
-    	console.log(this.props.children);
-	    //return React.cloneElement(this.props.children, {pair: pair});
-	    return (
-	    	<div>
-			    {this.props.children}
-		    </div>
-	    );
+	    return <Voting pair={pair}></Voting>
     }
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,25 @@
 /**引入React**/
 import React from 'react';
 import ReactDOM from 'react-dom';
-/**
- * 此处不引入react-router
- * 将使用BrowserRouter代替Router
- * react-router：https://reacttraining.com/react-router/web/api/Route
- */
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+/**此处不引入react-router**/
+import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom';
+
 
 /**引入组件**/
-import App from './components/App';//根路由组件
-import Results from './components/Results';//结果组件
-import Voting from './components/Voting';//投票组件
+import App from './components/App';
+import Results from './components/Results';
 
-/**
- * React Native4嵌套路由
- * 新路由写法：https://stackoverflow.com/questions/42095600/nested-routes-in-react-router-v4
- * this.props.children写法：http://jingyan.baidu.com/article/6f2f55a178f0f6b5b93e6c04.html
- */
 
-const routers=<Router>
-	<App>
-        <Route path="/results" component={Results} />
-    </App>
-</Router>
-
-ReactDOM.render( routers, document.getElementById('app'));
+/**RN4嵌套路由**/
+const Main = () => (
+    <Switch>
+		<Route exact path='/' render={(props) => {
+			return <App></App>
+		}}/>
+		<Route path='/results' component={Results}/>
+    </Switch>
+);
+/*渲染路由组件*/
+ReactDOM.render(<HashRouter>
+    <Main />
+  </HashRouter>, document.getElementById('app'));
